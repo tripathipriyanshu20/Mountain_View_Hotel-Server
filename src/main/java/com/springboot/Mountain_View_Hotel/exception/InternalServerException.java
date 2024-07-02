54a24ad22e4e9900139718418256a6837a3afd10
tripathipriyanshu20 <2k.priyanshutripathi@gmail.com>
2024-07-03 01:23:14 +0530

    commit third

---
+++ src/main/java/com/springboot/Mountain_View_Hotel/exception/InternalServerException.java
@@ -0,0 +1,7 @@
+package com.springboot.Mountain_View_Hotel.exception;
+
+public class InternalServerException extends Throwable {
+    public InternalServerException(String message) {
+        super(message);
+    }
+}
